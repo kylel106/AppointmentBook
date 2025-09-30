9b9dab0ab65e73c93fd5f3160ed60f61eca5c647
Kyle Landman <kylel106@nycstudents.net>
2025-09-30 15:33:13 -0400

    finished

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main (String[] args) {
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for (int i = 10; i < 15; i++) schedule[1][i] = true;
+        a.printPeriod(2);
+        a.findFreeBlock(2, 15);
+        int period = 2;
+        while (period < 5) {
+            System.out.println("Period: " + period);
+            a.printPeriod(period);
+            period++;
+        }
+        a.printPeriod(4);
+        System.out.println(a.makeAppointment(2, 4, 22));
+        a.printPeriod(4);
+    }
+}
